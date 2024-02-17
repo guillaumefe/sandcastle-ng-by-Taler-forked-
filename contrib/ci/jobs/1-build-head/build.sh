02061b7ef3dde0d0d45801cd761712726f0e6f6d
Devan Carpenter <devan@taler.net>
2024-02-17 11:05:32 -0500

    ci: fix typo

--- contrib/ci/jobs/1-build-head/build.sh
+++ contrib/ci/jobs/1-build-head/build.sh
@@ -9,7 +9,7 @@ done
 
 mkdir -p /artifacts/sandcastle-ng/${CI_COMMIT_REF} # Variable comes from CI environment
 podman tag taler-base-all:latest taler-base-all-head:latest
-podman tag taler-base-head:latest taler-base-all-head:${CI_COMMIT_REF}
+podman tag taler-base-all-head:latest taler-base-all-head:${CI_COMMIT_REF}
 podman save \
 	-o /artifacts/sandcastle-ng/${CI_COMMIT_REF}/taler-base-all-head.tar \
 	taler-base-all-head:latest
