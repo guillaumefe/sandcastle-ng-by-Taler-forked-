b54fd91f36bfa4679cbb265a410798d16199e12c
Devan Carpenter <devan@taler.net>
2024-02-16 23:12:28 -0500

    ci: version the saved image

--- contrib/ci/jobs/0-build/build.sh
+++ contrib/ci/jobs/0-build/build.sh
@@ -4,6 +4,7 @@ set -exuo pipefail
 ./sandcastle-build
 
 mkdir -p /artifacts/sandcastle-ng/${CI_COMMIT_REF} # Variable comes from CI environment
+podman tag taler-base-all:latest taler-base-all:${CI_COMMIT_REF}
 podman save \
-	-o /artifacts/sandcastle-ng/${CI_COMMIT_REF}/taler-base-all.tar \
-	taler-base-all:latest
+	-o /artifacts/sandcastle-ng/${CI_COMMIT_REF}/taler-base-all-${CI_COMMIT_REF}.tar \
+	taler-base-all:${CI_COMMIT_REF}
