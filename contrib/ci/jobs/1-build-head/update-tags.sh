230537beeabafbb208ed4cb280c7ba4aeb534ff4
Devan Carpenter <devan@taler.net>
2024-02-18 12:49:51 -0500

    head-ci: fill checkout refs with sha hash of HEAD

--- contrib/ci/jobs/1-build-head/update-tags.sh
+++ contrib/ci/jobs/1-build-head/update-tags.sh
@@ -13,10 +13,19 @@ MERCHANT_DEMOS_HEAD=$(fetch_head "git://git.taler.net/taler-merchant-demos")
 WALLET_HEAD=$(fetch_head "git://git.taler.net/wallet-core")
 SYNC_HEAD=$(fetch_head "git://git.taler.net/sync")
 
-echo $GNUNET_HEAD > buildconfig/gnunet.tag
-echo $EXCHANGE_HEAD > buildconfig/exchange.tag
-echo $MERCHANT_HEAD > buildconfig/merchant.tag
-echo $LIBEUFIN_HEAD > buildconfig/libeufin.tag
-echo $MERCHANT_DEMOS_HEAD > buildconfig/merchant-demos.tag
-echo $WALLET_HEAD > buildconfig/wallet.tag
-echo $SYNC_HEAD > buildconfig/sync.tag
+echo "master" > buildconfig/gnunet.tag
+echo "master" > buildconfig/exchange.tag
+echo "master" > buildconfig/merchant.tag
+echo "master" > buildconfig/libeufin.tag
+echo "master" > buildconfig/merchant-demos.tag
+echo "master" > buildconfig/wallet.tag
+echo "master" > buildconfig/sync.tag
+
+rm -f buildconfig/*.checkout
+echo $GNUNET_HEAD > buildconfig/gnunet.checkout
+echo $EXCHANGE_HEAD > buildconfig/exchange.checkout
+echo $MERCHANT_HEAD > buildconfig/merchant.checkout
+echo $LIBEUFIN_HEAD > buildconfig/libeufin.checkout
+echo $MERCHANT_DEMOS_HEAD > buildconfig/merchant-demos.checkout
+echo $WALLET_HEAD > buildconfig/wallet.checkout
+echo $SYNC_HEAD > buildconfig/sync.checkout
