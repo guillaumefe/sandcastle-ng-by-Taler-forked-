cc1f406b2e5545fadf8707055178559331dd6bcd
guillaumefe <guillaumefe@GNUTALER>
2024-04-30 12:15:40 +0200

    Remove all files to cleanly fork the project afterwards.

--- contrib/ci/jobs/0-build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -exuo pipefail
-
-./sandcastle-build
-
-echo "CI_COMMIT_REF = ${CI_COMMIT_REF}"
-mkdir -p /artifacts/sandcastle-ng/${CI_COMMIT_REF} # Variable comes from CI environment
-podman tag taler-base-all:latest taler-base-all:${CI_COMMIT_REF}
--- contrib/ci/jobs/1-build-head/job.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -exuo pipefail
-
-job_dir=$(dirname "${BASH_SOURCE[0]}")
-
-"${job_dir}"/build.sh
